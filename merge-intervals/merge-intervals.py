81372177e52fcec6c47450faad810697f1cc5a76
Tatiana <sizovatatiana1210@mail.ru>
2020-10-17 14:10:20 +0300

    LEETCODE-1021 Added merge-intervals.py

---
+++ merge-intervals/merge-intervals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        i = 0
+        intervals.sort()
+        while i < len(intervals) - 1:
+            if intervals[i][-1] >= intervals[i+1][0]:
+                if intervals[i+1][-1] < intervals[i][-1]:
+                    intervals.append([intervals[i][0], intervals[i][-1]])
+                else:
+                    intervals.append([intervals[i][0], intervals[i+1][-1]])
+                del intervals[i+1]
+                del intervals[i]
+                intervals.sort()
+                i = i - 1
+            i = i + 1
+        intervals.sort()
+        return intervals

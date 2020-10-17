433f0ce623712ad1fffdac4fca4d7620137ed295
Tatiana <sizovatatiana1210@mail.ru>
2020-10-17 23:21:17 +0300

    LEETCODE-1021 Added intersection-of-two-linked-lists.py

---
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        lenA, lenB = 0, 0
+        A, B = headA, headB
+
+        while (A is not None):
+            A = A.next
+            lenA = lenA + 1
+        while (B is not None):
+            B = B.next
+            lenB = lenB + 1
+
+        if (lenA < lenB):
+            while (lenA != lenB):
+                headB = headB.next
+                lenB = lenB - 1
+        if (lenB < lenA):
+            while (lenA != lenB):
+                headA = headA.next
+                lenA = lenA - 1
+
+        while (headA is not None or headB is not None):
+            if (headA == headB):
+                return headA
+            else:
+                headA = headA.next
+                headB = headB.next

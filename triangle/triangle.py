a6f2a6fc605edef1f8084fceaaf3cfaf931e0259
Tatiana <sizovatatiana1210@mail.ru>
2020-11-21 12:54:55 +0300

    LEETCODE-1021 Added def __init__, def get_all_lines

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -7,7 +7,11 @@ class Triangle:
     pass
 
 class File:
-    pass
+    def __init__(self, filename):
+        file = open(filename)
+
+    def get_all_lined(self):
+        return ['0 0 1 0 0 1']
 
 def main():
     pass
